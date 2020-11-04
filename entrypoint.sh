504f7f42724b562253eb843f0949095661612923
Jason Williams <jasonw@jhu.edu>
2020-11-04 15:07:02 -0500

    Remove the check for a GITHUB_TOKEN Fixes #43

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,20 @@
 
 # just in case, set $HOME to / in the container.
 export HOME=/
+# TODO: Not currently neede? Remove.
+# # A way to see if we are running from GitHub actions...
+# if [ ! -z ${GITHUB_WORKSPACE+x} ]
+# then
 
-# A way to see if we are running from GitHub actions...
-if [ ! -z ${GITHUB_WORKSPACE+x} ]
-then
-
-  # GITHUB_WORKSPACE is set, let's check for  a token
-  if [ -z ${GITHUB_TOKEN+x} ]
-  then
-    # No token, error out.
-    echo "Error: Unable to get GITHUB_TOKEN" >&2
-    echo "Error: Please make sure you set a GITHUB_TOKEN" >&2
-    exit 1
-  fi
-fi
+#   # GITHUB_WORKSPACE is set, let's check for  a token
+#   if [ -z ${GITHUB_TOKEN+x} ]
+#   then
+#     # No token, error out.
+#     echo "Error: Unable to get GITHUB_TOKEN" >&2
+#     echo "Error: Please make sure you set a GITHUB_TOKEN" >&2
+#     exit 1
+#   fi
+# fi
 
 # if GITHUB_WORKSPACE is set, use it.  Otherwise default to /tmp/code
 GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-/tmp/code}
